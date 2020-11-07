098e4bf3cfec29d8755b1c2f70b857410552feab
Vadim Gratsilev <vadim.gratsilev@gmail.com>
2020-11-07 17:40:28 +0500

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Navbar } from './components/Navbar/Navbar';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Profile } from './pages/Profile';
+import { About } from './pages/About';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <div className={'container pt-4'}>
+                <Switch>
+                    <Route path={'/'} exact component={Home} />
+                    <Route path={'/about'} component={About} />
+                    <Route path={'/profile/:name'} component={Profile} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const About = () => {
+
+    return (
+        <div>
+            <div className='jumbotron'>
+                <div className='container'>
+                    <h1 className='display-4'>About</h1>
+                    <p className='lead'>React hooks app</p>
+                </div>
+            </div>
+        </div>
+    )
+}
