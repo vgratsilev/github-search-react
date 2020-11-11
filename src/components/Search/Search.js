ca00a9a9ef5495ae1266800591a956816144dfcb
Vadim Gratsilev <vadim.gratsilev@gmail.com>
2020-11-11 21:17:57 +0500

    Added state to Search component.

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,12 +1,20 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AlertContext } from '../../context/alert/alertContext';
 
 export const Search = () => {
+    const [value, setValue] = useState('');
     const {show} = useContext(AlertContext);
 
     const onSubmit = (event) => {
-        if (event.key === 'Enter') {
-            show('Alert message');
+        if (event.key !== 'Enter') {
+           return;
+        }
+
+        if (value.trim()) {
+            console.log(`Search with: ${value}`);
+        }
+        else {
+            show('Input username');
         }
     };
     return (
@@ -16,6 +24,9 @@ export const Search = () => {
                 className={'form-control'}
                 placeholder={'Username'}
                 onKeyPress={onSubmit}
+                value={value}
+
+                onChange={event => setValue(event.target.value)}
             />
         </div>
     )
