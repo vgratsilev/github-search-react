41f826f559bae19a38980514067f00bda98418e4
Vadim Gratsilev <vadim.gratsilev@gmail.com>
2020-11-11 21:52:50 +0500

    Added GithubState context value.

--- src/context/github/githubState.js
+++ src/context/github/githubState.js
@@ -1,6 +1,7 @@
 import React, { useReducer } from 'react';
 import { GithubContext } from './githubContext';
 import { GithubReducer } from './githubReducer';
+import { CLEAR_USERS, GET_REPOS, GET_USER, SEARCH_USERS, SET_LOADING } from '../types';
 
 export const GithubState = ({children}) => {
     const initialState = {
@@ -11,8 +12,55 @@ export const GithubState = ({children}) => {
     };
     const [state, dispatch] = useReducer(GithubReducer, initialState);
 
+    const searchUsers = async (value) => {
+        setLoading();
+        //axios get
+
+        dispatch({
+            type: SEARCH_USERS,
+            payload: []
+        });
+    };
+
+    const getUser = async (value) => {
+        setLoading();
+        //axios get
+
+        dispatch({
+            type: GET_USER,
+            payload: {}
+        });
+    };
+
+    const getRepos = async (value) => {
+        setLoading();
+        //axios get
+
+        dispatch({
+            type: GET_REPOS,
+            payload: []
+        });
+    };
+
+    const clearUsers = async () => {
+        dispatch({
+            type: CLEAR_USERS
+        });
+    };
+
+    const setLoading = () => {
+        dispatch({
+            type: SET_LOADING
+        });
+    };
+
+    const {user, users, loading, repos} = state;
+
     return (
-        <GithubContext.Provider value={{}}>
+        <GithubContext.Provider value={{
+            searchUsers, getUser, getRepos, clearUsers, setLoading,
+            user, users, loading, repos
+        }}>
             {children}
         </GithubContext.Provider>
     );
